0a0bdce2882926ecbd1abf8240952257adab9379
Azzeddine Iqachaden <pandonyx@gmail.com>
2025-09-05 22:33:59 +0100

    django templates

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -8,15 +8,15 @@ class Author(models.Model):
         return self.name
 
 
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)  # Many books → one author
-    library = models.ForeignKey(Library, on_delete=models.CASCADE, related_name="books")  # Many books → one library
+
 
 
     def __str__(self):
         return self.title
-
 
 class Library(models.Model):
     name = models.CharField(max_length=100)
