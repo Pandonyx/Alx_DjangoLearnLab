94229d73a5587f75818455986b27ee4c1555d363
Azzeddine Iqachaden <pandonyx@gmail.com>
2025-09-30 20:32:30 +0100

    Add initial CSS and JavaScript files for styling and alert functionality

---
+++ django_blog/staticfiles/blog/js/main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Django Blog loaded successfully!");
+
+  const alerts = document.querySelectorAll(".alert");
+  alerts.forEach((alert) => {
+    setTimeout(() => {
+      alert.style.transition = "opacity 0.5s";
+      alert.style.opacity = "0";
+      setTimeout(() => alert.remove(), 500);
+    }, 5000);
+  });
+});
