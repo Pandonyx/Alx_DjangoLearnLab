6e8c7cefc46f746561499af91fd0000977758bc0
Azzeddine Iqachaden <pandonyx@gmail.com>
2025-10-01 02:01:58 +0100

    Implement CRUD functionality for blog posts with corresponding views, templates, and forms

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,11 +1,28 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from users import views as user_views
-from . import views
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    home,
+    about
+)
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
-    path('about/', views.about, name='blog-about'),
+    path('', home, name='blog-home'),
+    path('about/', about, name='blog-about'),
+    
+    # Post CRUD URLs
+    path('posts/', PostListView.as_view(), name='post-list'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('posts/new/', PostCreateView.as_view(), name='post-create'),
+    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    
+    # Authentication URLs
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 from .models import Post
 
 
@@ -15,3 +22,55 @@ def home(request):
 def about(request):
     """View for the about page"""
     return render(request, 'blog/about.html', {'title': 'About'})
+
+
+class PostListView(ListView):
+    """Display all blog posts"""
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-published_date']
+    paginate_by = 5
+
+
+class PostDetailView(DetailView):
+    """Display a single blog post"""
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    """Allow authenticated users to create new posts"""
+    model = Post
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'content']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """Allow post authors to edit their posts"""
+    model = Post
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'content']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    """Allow post authors to delete their posts"""
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = '/posts/'
+    
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
