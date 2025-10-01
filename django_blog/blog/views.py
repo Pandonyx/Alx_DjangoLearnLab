0b37e69c053d429fa4fe25d6e640adff58726011
Azzeddine Iqachaden <pandonyx@gmail.com>
2025-10-01 02:05:11 +0100

    Add login_required decorator to views.py for enhanced access control

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
     ListView,
